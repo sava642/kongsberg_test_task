e8c29236c53247275333ce20751d27ddd9b1a1d9
sava642 <101993338+sava642@users.noreply.github.com>
2023-06-18 14:57:52 +0200

    test

--- src/components/HomePage/HomePage.test.tsx
+++ src/components/HomePage/HomePage.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './HomePage';
 
